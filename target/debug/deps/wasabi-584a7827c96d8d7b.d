ce0ff850c8047ef09d4311ce0623a7796d078688
beso1225 <yutaroformisc@gmail.com>
2026-02-28 16:20:28 +0900

    add rust project

---
+++ target/debug/deps/wasabi-584a7827c96d8d7b.d
@@ -0,0 +1,5 @@
+/Users/yutarotakagi/Documents/programing/Rust/Study/OS/wasabi/target/debug/deps/wasabi-584a7827c96d8d7b.d: src/main.rs
+
+/Users/yutarotakagi/Documents/programing/Rust/Study/OS/wasabi/target/debug/deps/libwasabi-584a7827c96d8d7b.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/wasabi-cd578dc43329db43.d
@@ -0,0 +1,5 @@
+/Users/yutarotakagi/Documents/programing/Rust/Study/OS/wasabi/target/debug/deps/wasabi-cd578dc43329db43.d: src/main.rs
+
+/Users/yutarotakagi/Documents/programing/Rust/Study/OS/wasabi/target/debug/deps/libwasabi-cd578dc43329db43.rmeta: src/main.rs
+
+src/main.rs:
