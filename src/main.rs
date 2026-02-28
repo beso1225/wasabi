da04664e37c2281d7264bd03df32753ba9de641f
beso1225 <yutaroformisc@gmail.com>
2026-02-28 17:06:57 +0900

    dev: update efi_main function signature and implement VRAM handling

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@
 #![no_main]
 
 #[unsafe(no_mangle)]
-fn efi_main() {
+fn efi_main(_image_hangle: EfiHandle, efi_system_table: &EfiSystemTable) {
+    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
+    let vram = unsafe {
+        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
+    };
     // println!("Hello, world!");
     loop {}
 }
 
-use core::panic::PanicInfo;
+use core::{panic::PanicInfo, slice};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
