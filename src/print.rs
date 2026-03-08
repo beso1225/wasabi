aa03ac9db51143b187f1cd88e8741830fb957940
beso1225 <yutaroformisc@gmail.com>
2026-03-08 22:41:26 +0900

    add: print! macro

---
+++ src/print.rs
@@ -0,0 +1,15 @@
+use core::fmt;
+
+use crate::serial::SerialPort;
+
+pub fn global_print(args: fmt::Arguments) {
+    let mut writer = SerialPort::default();
+    fmt::write(&mut writer, args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($args:tt)*) => {
+        ($crate::print::global_print(format_args!($($args)*)));
+    };
+}
