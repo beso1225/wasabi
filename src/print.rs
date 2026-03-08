a1ad03de41eda9dfb363486a2fd7eb7215bece9c
beso1225 <yutaroformisc@gmail.com>
2026-03-08 23:07:37 +0900

    add: hexdump debugger function

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,4 @@
-use core::fmt;
+use core::{fmt, mem::size_of, slice};
 
 use crate::serial::SerialPort;
 
@@ -39,4 +39,60 @@ macro_rules! error {
     ($($arg:tt)*) => {
         ($crate::print!("[ERROR] {}:{:<3}: {}\n", file!(), line!(), format_args!($($arg)*)))
     };
+}
+
+fn hexdump_bytes(bytes: &[u8]) {
+    let mut i = 0;
+    let mut ascii = [0u8; 16];
+    let mut offset = 0;
+    for v in bytes.iter() {
+        if i == 0 {
+            print!("{:08X}: ", offset);
+        }
+        print!("{:02X} ", v);
+        ascii[i] = *v;
+        i += 1;
+        if i == 16 {
+            print!("|");
+            for c in ascii.iter() {
+                print!(
+                    "{}",
+                    match c {
+                        0x20..=0x7e => {
+                            *c as char
+                        }
+                        _ => {
+                            '.'
+                        }
+                    }
+                );
+            }
+            println!("|");
+            offset += 16;
+            i = 0;
+        }
+    }
+    if i != 0 {
+        let old_i = i;
+        while i < 16 {
+            print!("   ");
+            i += 1;
+        }
+        print!("|");
+        for c in ascii[0..old_i].iter() {
+            print!(
+                "{}",
+                if (0x20u8..=0x7fu8).contains(c) {
+                    *c as char
+                } else {
+                    '.'
+                }
+            );
+        }
+        println!("|");
+    }
+}
+
+pub fn hexdump<T: Sized>(data: &T) {
+    hexdump_bytes(unsafe { slice::from_raw_parts(data as *const T as *const u8, size_of::<T>()) });
 }
