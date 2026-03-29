ac803234309645b0d61576c5dc92053876b0f829
beso1225 <yutaroformisc@gmail.com>
2026-03-30 00:30:51 +0900

    feat: !!! stil writing !!! adding paging interruptions and exceptions handling

--- src/x86.rs
+++ src/x86.rs
@@ -1,6 +1,11 @@
-use core::{arch::asm, fmt, marker::PhantomData};
+use core::{
+    arch::{asm, global_asm},
+    fmt,
+    marker::PhantomData,
+    mem::size_of,
+};
 
-use crate::result::Result;
+use crate::{error, result::Result};
 
 pub fn hlt() {
     unsafe { asm!("hlt") }
@@ -139,3 +144,359 @@ pub type PT = Table<1, 12, [u8; PAGE_SIZE]>;
 pub type PD = Table<2, 21, PT>;
 pub type PDPT = Table<3, 30, PD>;
 pub type PML4 = Table<4, 39, PDPT>;
+
+/// # Safety
+/// Anthing can happen if the given selector is invalid.
+pub unsafe fn write_es(selector: u16) {
+    asm!(
+        "mov es, ax",
+        in("ax") selector
+    )
+}
+
+/// # Safety
+/// Anything can happen if the CS given is invalid.
+pub unsafe fn wrtie_cs(cs: u16) {
+    // The MOV instruction CANNOT be used to load the CS register.
+    // Use far-jump(ljmp) instead
+    asm!(
+        "lea rax, [rip + 2f]", // Target address (label 1 below)
+        "push cx",             // Construct a far pointer on the stack
+        "push rax",
+        "ljmp [rsp]",
+        "2:",
+        "add rsp, 8 + 2", // Cleanup the far pointer on the stack
+        in("cx") cs
+    )
+}
+
+/// # Safety
+/// Anything can happen if the gicen selector is invalid.
+pub unsafe fn write_ss(selector: u16) {
+    asm!(
+        "mov ss, ax",
+        in("ax") selector
+    )
+}
+
+/// # Safety
+/// Anything can happen if the gicen selector is invalid.
+pub unsafe fn write_ds(selector: u16) {
+    asm!(
+        "mov ds, ax",
+        in("ax") selector
+    )
+}
+
+/// # Safety
+/// Anything can happen if the gicen selector is invalid.
+pub unsafe fn write_fs(selector: u16) {
+    asm!(
+        "mov fs, ax",
+        in("ax") selector
+    )
+}
+
+/// # Safety
+/// Anything can happen if the gicen selector is invalid.
+pub unsafe fn write_gs(selector: u16) {
+    asm!(
+        "mov ss, ax",
+        in("ax") selector
+    )
+}
+
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct FPUContext {
+    data: [u8; 512],
+}
+
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct GeneralRegisterContext {
+    rax: u64,
+    rdx: u64,
+    rbx: u64,
+    rbp: u64,
+    rsi: u64,
+    rdi: u64,
+    r8: u64,
+    r9: u64,
+    r10: u64,
+    r11: u64,
+    r12: u64,
+    r13: u64,
+    r14: u64,
+    r15: u64,
+    rcx: u64,
+}
+const _: () = assert!(size_of::<GeneralRegisterContext>() == (16 - 1) * 8);
+
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+struct InterruptContext {
+    rip: u64,
+    cs: u64,
+    rfalgs: u64,
+    rsp: u64,
+    ss: u64,
+}
+const _: () = assert!(size_of::<InterruptContext>() == 8 * 5);
+
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct InterruptInfo {
+    // This struct is placed at the top of the interrupt stack.
+    // Should be aligned on 16-byte boundaries to pass the alignment checks done by FXSAVE / FXRSTOR
+    fpu_context: FPUContext, // used by FXSAVE / FXRSTOR
+    _dummy: u64,
+    greg: GeneralRegisterContext,
+    error_code: u64,
+    ctx: InterruptContext,
+}
+const _: () = assert!(size_of::<InterruptInfo>() == (16 + 4 + 1) * 8 + 8 + 512);
+
+impl fmt::Debug for InterruptInfo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "
+        {{
+            rip: {:#018X}, CS: {:#06X},
+            rsp: {:#018X}, SS: {:#06X},
+            rbp: {:#018X},
+
+            rflags:     {:#018X},
+            error_code: {:#018X},
+
+            rax: {:#018X}, rcx: {:#018X},
+            rdx: {:#018X}, rbx: {:#018X},
+            rsi: {:#018X}, rdi: {:#018X},
+            r8:  {:#018X}, r9:  {:#018X},
+            r10: {:#018X}, r11: {:#018X},
+            r12: {:#018X}, r13: {:#018X},
+            r14: {:#018X}, r15: {:#018X},
+        }}",
+            self.ctx.rip,
+            self.ctx.cs,
+            self.ctx.rsp,
+            self.ctx.ss,
+            self.greg.rbp,
+            self.ctx.rfalgs,
+            self.error_code,
+            //
+            self.greg.rax,
+            self.greg.rcx,
+            self.greg.rdx,
+            self.greg.rbx,
+            //
+            self.greg.rsi,
+            self.greg.rdi,
+            //
+            self.greg.r8,
+            self.greg.r9,
+            self.greg.r10,
+            self.greg.r11,
+            self.greg.r12,
+            self.greg.r13,
+            self.greg.r14,
+            self.greg.r15,
+        )
+    }
+}
+
+// SDM Vol.3: 6.14.2 64-bit Mode Stack Frame
+// In IA-32e mod, the RSP is aligned ro a 16-byte boundary
+// before pushing the stack fram.
+
+/// This generates interrupt_entrypointN()
+/// Generated asm will be looks like this:
+/// ```
+/// .global interrupt_entrypointN
+///     interrupt_entrypointN:
+///     push 0 // No error code
+///     push rcx // Save rcx first to reuse
+///     mov rcx, N // INT#`
+///     imp inthandler_common
+/// ```
+macro_rules! interrupt_entrypoint {
+    ($index:literal) => {
+        global_asm!(concat!(
+            ".global interrupt_entrypoint",
+            stringify!($index),
+            "\n",
+            "interrupt_entrypoint",
+            stringify!($index),
+            ":\n",
+            "push 0 // No error code\n",
+            "push rcx // Save rcx first to reuse\n",
+            "mov rcx, ",
+            stringify!($index),
+            "\n",
+            "jmp inthandler_common\n"
+        ));
+    };
+}
+macro_rules! interrupt_entrypoint_with_error_code {
+    ($index:literal) => {
+        global_asm!(concat!(
+            ".global interrupt_entrypoint",
+            stringify!($index),
+            "\n",
+            "interrupt_entrypoint",
+            stringify!($index),
+            ":\n",
+            "push rcx // Save rcx first to reuse\n",
+            "mov rcx, ",
+            stringify!($index),
+            "\n",
+            "jmp inthandler_common\n"
+        ));
+    };
+}
+
+interrupt_entrypoint!(3);
+interrupt_entrypoint!(6);
+interrupt_entrypoint_with_error_code!(8);
+interrupt_entrypoint_with_error_code!(13);
+interrupt_entrypoint_with_error_code!(14);
+interrupt_entrypoint!(32);
+
+extern "sysv64" {
+    fn interrupt_entrypoint3();
+    fn interrupt_entrypoint6();
+    fn interrupt_entrypoint8();
+    fn interrupt_entrypoint13();
+    fn interrupt_entrypoint14();
+    fn interrupt_entrypoint32();
+}
+
+global_asm!(
+    r#"
+.global inthandler_common
+inthandler_common:
+    // General purpose registers (except rsp and rcx)
+    push r15
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push rdi
+    push rsi
+    push rbp
+    push rbx
+    push rdx
+    push rax
+    // FPU State
+    sub rsp, 512 + 8
+    fxsave64[rsp]
+    // 1st parameter: pointer to the saved CPU state
+    mov rdi, rsp
+    // Align the stack to 16-bytes boundary
+    mov rbp, rsp
+    and rsp, -16
+    // 2nd parameter: Int#
+    mov rsi, rcx
+
+    call inthandler
+
+    mov rsp, rbp
+    //
+    fxrstor64[rsp]
+    add rsp, 512 + 8
+    //
+    pop rax
+    pop rdx
+    pop rbx
+    pop rbp
+    pop rsi
+    pop rdi
+    pop r8
+    pop r9
+    pop r10
+    pop r11
+    pop r12
+    pop r13
+    pop r14
+    pop r15
+    //
+    pop rcx
+    add rsp, 8 // for Error Code
+    iretq
+"#
+);
+
+pub fn read_cr2() -> u64 {
+    let mut cr2: u64;
+    unsafe {
+        asm!("mov rax, cr2",
+        out("rax") cr2)
+    }
+    cr2
+}
+
+#[no_mangle]
+extern "sysv64" fn inthandler(info: &InterruptInfo, index: usize) {
+    error!("interrupt Info: {:?}", info);
+    error!("Exeption {:#04X}: ", index);
+    match index {
+        3 => {
+            error!("Breakpoint");
+        }
+        6 => {
+            error!("Invalid Opcode");
+        }
+        8 => {
+            error!("Double Fault");
+        }
+        13 => {
+            error!("General Protection Fault");
+            let rip = info.ctx.rip;
+            error!("Bytes @ RIP({:#018X}):", rip);
+            let rip = rip as *const u8;
+            let bytes = unsafe { core::slice::from_raw_parts(rip, 16) };
+            error!(" = {:02X?}", bytes);
+        }
+        14 => {
+            error!("Page Fault");
+            error!("CR2={:#018X}", read_cr2());
+            error!(
+                "Caused by: A {} mode {} on a {} page, page structures are {}",
+                if info.error_code & 0b0000_0100 != 0 {
+                    "user"
+                } else {
+                    "supervisor"
+                },
+                if info.error_code & 0b0001_0000 != 0 {
+                    "instruction fetch"
+                } else if info.error_code & 0b0010 != 0 {
+                    "data write"
+                } else {
+                    "data read"
+                },
+                if info.error_code & 0b0001 != 0 {
+                    "present"
+                } else {
+                    "not present"
+                },
+                if info.error_code & 0b1000 != 0 {
+                    "invalid"
+                } else {
+                    "valid"
+                },
+            );
+        }
+        _ => {
+            error!("Not handled");
+        }
+    }
+    panic!("fatal exception");
+}
