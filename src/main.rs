0d1d13ba4d7cbbc053de05536e1fc5b5969924d1
beso1225 <yutaroformisc@gmail.com>
2026-06-20 22:53:36 +0900

    refactor: change textwriter to bitmap from vram

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use wasabi::executor::TimeoutFuture;
 use wasabi::graphics::draw_test_pattern;
 use wasabi::graphics::fill_rect;
 use wasabi::graphics::Bitmap;
+use wasabi::graphics::BitmapTextWriter;
 use wasabi::hpet::global_timestamp;
 use wasabi::info;
 use wasabi::init::init_allocator;
@@ -57,7 +58,7 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     hexdump(efi_system_table);
     let mut vram = init_vram(efi_system_table).expect("init_vram failed");
     init_display(&mut vram);
-    let mut w = VramTextWriter::new(&mut vram);
+    let mut w = BitmapTextWriter::new(&mut vram);
     let acpi = efi_system_table.acpi_table().expect("ACPI table not found");
     let memory_map = init_basic_runtime(image_handle, efi_system_table);
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
